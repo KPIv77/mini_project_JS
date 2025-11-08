75a47028a09a10b94b79bef3faa79a6ad433882e
KPIv77 <kitisakpholor@gmail.com>
2025-11-08 13:17:43 +0700

    Initial commit

---
+++ calculator/main_js/scpipt.js
@@ -0,0 +1,60 @@
+
+let expression = "";
+
+function showMessage(value) {
+    const messageEl = document.getElementById("message");
+
+    if ((!isNaN(value)) || (value === '.'))  {
+        expression += value
+        messageEl.innerText = expression;
+
+    } 
+    else if (value === "AC") {
+        expression = "";
+        messageEl.innerText = "";
+
+    } 
+    else if (value === 'Del') {
+        expression = expression.slice(0, -1);
+        messageEl.innerText = expression;
+
+    } 
+    else if (value === 'x') {
+        expression += "*";
+        messageEl.innerText = expression;
+
+    } 
+    else if (value === '+') {
+        expression += "+";
+        messageEl.innerText = expression;
+    }
+
+    else if (value === '/') {
+        expression += "/";
+        messageEl.innerText = expression;
+    }
+
+    else if (value === '-') {
+        expression += "-";
+        messageEl.innerText = expression;
+    }
+
+    else if (value === '%') {
+        expression = (parseFloat(expression) / 100).toString();
+        messageEl.innerText = expression;
+    }
+
+    
+
+    else if (value === "=") {
+        try {
+            expression = eval(expression).toString();
+            messageEl.innerText = expression;
+
+        } catch {
+            messageEl.innerText = "Error";
+            expression = "";
+        }
+    }
+}
+
