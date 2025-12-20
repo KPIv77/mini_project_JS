8f50b9851992b2ae045fc42763d0665e5fcb86ff
KPIv77 <kitisakpholor@gmail.com>
2025-12-20 17:59:15 +0700

    revise func fade page

--- digital_clock/clock.js
+++ digital_clock/clock.js
@@ -11,6 +11,9 @@ function time_update() {
 
 // Show clock page (page 1)
 function clock_page() {
+
+    const timer_class = document.querySelector('.timers div span');
+    const stopwatch_class = document.querySelector('.lab_start span');
     const shtime_Btn = document.querySelector('#shtime_Btn'); 
 
     shtime_Btn.addEventListener('click', () => {
@@ -21,14 +24,26 @@ function clock_page() {
 
         // show page clock
         document.querySelector(".showtime").classList.remove("hidden");
+
+        
+        // hide lab start and timers class
+        timer_class.style.pointerEvents = 'none';
+        timer_class.style.cursor = 'default';
+
+        stopwatch_class.style.pointerEvents = 'none';
+        stopwatch_class.style.cursor = 'default';
+
+        document.querySelector(".lab_stop_grid").classList.remove("show", "button_show");
+
+
     });
 }
 
 // Show Stop watch page (page 2)
 function stop_watch () {
 
+    const stopwatch_class = document.querySelector('.lab_start span');
     const stop_Btn = document.querySelector('#stop_Btn');
-
 
     stop_Btn.addEventListener('click', () => {
 
@@ -39,14 +54,46 @@ function stop_watch () {
         // show page stopwatch page
         document.querySelector(".stopwatch_page").classList.add("show");
 
-        // add button lab_start
-        document.querySelector(".lab_start span").classList.add("button_show");
+        // add button lab_start class remove timers class
+
+        document.querySelector('.timers div').classList.add('hidden');
+        document.querySelector('.lab_start').classList.remove('hidden');
+
+        stopwatch_class.style.pointerEvents = 'auto';
+        stopwatch_class.style.cursor = 'pointer';
 
     });
 }
 
+// Show grid class lab_stop_grid
+function grid_lab_stop () {
+
+    const grid_lab_stop_class = document.querySelector('.lab_stop_grid span');
+    const start_Btn = document.querySelector("#start");
+    const stopwatch_class = document.querySelector('.lab_start span');
+
+    start_Btn.addEventListener('click', () => {
+
+        console.log("tets");
+
+        document.querySelector(".reset_start_grid").classList.add("hidden");
+        document.querySelector(".lab_start").classList.add("hidden");
+        document.querySelector(".lab_stop_grid").classList.add("show");
+        document.querySelector(".lab_stop_grid span").classList.add("button_show");
+
+        stopwatch_class.style.pointerEvents = 'none';
+        stopwatch_class.style.cursor = 'default';
+
+
+    });
+
+
+}
+
 // Show timer page (page 3)
 function timer_page () {
+
+    const timer_class = document.querySelector('.timers div span');
     const timers_Btn = document.querySelector('#timers_Btn');
 
     timers_Btn.addEventListener('click', () => {
@@ -58,8 +105,15 @@ function timer_page () {
         // show page timers page
         document.querySelector(".timers").classList.add("show");
 
+        // add button timers class remove lab_start class
 
-        
+        document.querySelector('.lab_start').classList.add('hidden');
+        document.querySelector('.timers div').classList.remove('hidden');
+
+        timer_class.style.pointerEvents = 'auto';
+        timer_class.style.cursor = 'pointer';
+
+    document.querySelector(".lab_stop_grid").classList.remove("show", "button_show");
 
     });
 }
@@ -85,5 +139,6 @@ document.addEventListener("DOMContentLoaded", () => {
     stop_watch();
     timer_page();
     button_color_click();
+    grid_lab_stop();
 
 });
